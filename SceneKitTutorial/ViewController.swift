e5309a2fbf1b03d9425c10a1c1e52e5e04250cde
krzychukula <krzychukula@gmail.com>
2014-12-20 17:26:33 +0100

    défaut camera with auto gestures

--- SceneKitTutorial/ViewController.swift
+++ SceneKitTutorial/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
         
         
         scnView.autoenablesDefaultLighting = true
+        scnView.allowsCameraControl = true
     }
 
     override func didReceiveMemoryWarning() {
